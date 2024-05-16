c56f846b0dc2993492c3f4bd49f144664d95ef04
이창형 <71262367+LeeChangHyeong@users.noreply.github.com>
2024-05-16 23:40:51 +0900

    ✨ update - 조회된 일정 목록을 작성일 기준 내림차순으로 정렬하는 메서드 생성

--- src/main/java/org/example/owncalendarserver/repository/ScheduleRepository.java
+++ src/main/java/org/example/owncalendarserver/repository/ScheduleRepository.java
@@ -3,5 +3,8 @@ package org.example.owncalendarserver.repository;
 import org.example.owncalendarserver.entity.Schedule;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
+    List<Schedule> findAllByOrderByCreateDateDesc();
 }
