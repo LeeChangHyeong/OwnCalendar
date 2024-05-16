59775396c692471f223cb70a1f9253058e0b4b5b
이창형 <71262367+LeeChangHyeong@users.noreply.github.com>
2024-05-16 22:14:33 +0900

    ✨ update - 프론트가 서버한테서 받을 dto 구현

--- src/main/java/org/example/owncalendarserver/dto/ScheduleResponseDto.java
+++ src/main/java/org/example/owncalendarserver/dto/ScheduleResponseDto.java
@@ -1,4 +1,23 @@
 package org.example.owncalendarserver.dto;
 
+import lombok.Getter;
+import org.example.owncalendarserver.entity.Schedule;
+
+import java.util.Date;
+
+@Getter
 public class ScheduleResponseDto {
+    private Long id;
+    private String title;
+    private String content;
+    private String name;
+    private Date createDate;
+
+    public ScheduleResponseDto(Schedule schedule) {
+        this.id = schedule.getId();
+        this.title = schedule.getTitle();
+        this.content = schedule.getContent();
+        this.name = schedule.getName();
+        this.createDate = schedule.getCreateDate();
+    }
 }
