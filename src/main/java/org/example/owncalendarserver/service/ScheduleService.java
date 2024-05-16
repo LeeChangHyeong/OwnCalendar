954b1a46f9e7ce0c69e12d2a81729e6727b1bc24
이창형 <71262367+LeeChangHyeong@users.noreply.github.com>
2024-05-16 22:15:11 +0900

    ✨ update - service 클래스에서 schedule을 저장해주는 메서드 구현

--- src/main/java/org/example/owncalendarserver/service/ScheduleService.java
+++ src/main/java/org/example/owncalendarserver/service/ScheduleService.java
@@ -1,8 +1,32 @@
 package org.example.owncalendarserver.service;
 
+import org.example.owncalendarserver.dto.ScheduleRequestDto;
+import org.example.owncalendarserver.dto.ScheduleResponseDto;
+import org.example.owncalendarserver.entity.Schedule;
+import org.example.owncalendarserver.repository.ScheduleRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ScheduleService {
 
+    private final ScheduleRepository scheduleRepository;
+
+    // 생성자로 주입
+    public ScheduleService(ScheduleRepository scheduleRepository) {
+        this.scheduleRepository = scheduleRepository;
+    }
+
+
+    public ScheduleResponseDto createSchedule(ScheduleRequestDto requestDto) {
+        // RequestDto -> Entity
+        Schedule schedule = new Schedule(requestDto);
+
+        // DB 저장
+        Schedule saveSchedule = scheduleRepository.save(schedule);
+
+        // Entity -> ResponseDto
+        ScheduleResponseDto scheduleResponseDto = new ScheduleResponseDto(saveSchedule);
+
+        return scheduleResponseDto;
+    }
 }
