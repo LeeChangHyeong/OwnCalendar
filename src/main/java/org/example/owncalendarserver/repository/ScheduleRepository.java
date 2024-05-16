ebb025977e077b81bd3b4795ef28c063b0ef4716
이창형 <71262367+LeeChangHyeong@users.noreply.github.com>
2024-05-16 22:13:28 +0900

    ✨ update - Repository에 JpaRepository extends

--- src/main/java/org/example/owncalendarserver/repository/ScheduleRepository.java
+++ src/main/java/org/example/owncalendarserver/repository/ScheduleRepository.java
@@ -1,4 +1,7 @@
 package org.example.owncalendarserver.repository;
 
-public class ScheduleRepository {
+import org.example.owncalendarserver.entity.Schedule;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
 }
