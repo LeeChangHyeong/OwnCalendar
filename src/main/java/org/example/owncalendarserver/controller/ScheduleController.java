712a7c71467b8816b99b817ad503bdebff84083e
이창형 <71262367+LeeChangHyeong@users.noreply.github.com>
2024-05-16 22:12:56 +0900

    ✨ update - ScheduleController에서 스케쥴 저장하는 api 구현

--- src/main/java/org/example/owncalendarserver/controller/ScheduleController.java
+++ src/main/java/org/example/owncalendarserver/controller/ScheduleController.java
@@ -1,4 +1,25 @@
 package org.example.owncalendarserver.controller;
 
+import org.example.owncalendarserver.dto.ScheduleRequestDto;
+import org.example.owncalendarserver.dto.ScheduleResponseDto;
+import org.example.owncalendarserver.service.ScheduleService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
 public class ScheduleController {
+
+    private final ScheduleService scheduleService;
+
+    public ScheduleController(ScheduleService scheduleService) {
+        this.scheduleService = scheduleService;
+    }
+
+    @PostMapping("/schedule")
+    public ScheduleResponseDto createSchedule(@RequestBody ScheduleRequestDto requestDto) {
+        return scheduleService.createSchedule(requestDto);
+    }
 }
