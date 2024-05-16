d1309accd99602f8fd6cabe81ad85fdbcf7cbf79
이창형 <71262367+LeeChangHyeong@users.noreply.github.com>
2024-05-16 22:14:19 +0900

    ✨ update - 서버가 프론트한테서 받을 requestDto 구현

--- src/main/java/org/example/owncalendarserver/dto/ScheduleRequestDto.java
+++ src/main/java/org/example/owncalendarserver/dto/ScheduleRequestDto.java
@@ -1,4 +1,11 @@
 package org.example.owncalendarserver.dto;
 
+import lombok.Getter;
+
+@Getter
 public class ScheduleRequestDto {
+    private String title;
+    private String content;
+    private String name;
+    private String password;
 }
