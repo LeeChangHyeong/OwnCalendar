7b356c4525068bfe78c25e02b5a9a972701e1505
이창형 <71262367+LeeChangHyeong@users.noreply.github.com>
2024-05-16 19:01:01 +0900

    🎉 add - 프로젝트 생성

---
+++ src/main/java/org/example/owncalendarserver/OwnCalendarServerApplication.java
@@ -0,0 +1,13 @@
+package org.example.owncalendarserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OwnCalendarServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OwnCalendarServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/owncalendarserver/OwnCalendarServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.owncalendarserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OwnCalendarServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
