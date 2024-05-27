f9e3607ed071d95094a17349532eb962fc240235
이창형 <71262367+LeeChangHyeong@users.noreply.github.com>
2024-05-27 18:12:28 +0900

    🎉 add - Comment entity 생성

---
+++ src/main/java/org/example/owncalendarserver/entity/Comment.java
@@ -0,0 +1,29 @@
+package org.example.owncalendarserver.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Entity
+@Getter
+@Table
+@NoArgsConstructor
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "content", nullable = false)
+    private String content;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "createDate", nullable = false)
+    private Date createDate;
+}
