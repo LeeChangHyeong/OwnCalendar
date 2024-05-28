94ff7df177d7e0f0694dcc04e2a95082828f7dfc
이창형 <71262367+LeeChangHyeong@users.noreply.github.com>
2024-05-28 22:16:26 +0900

    ✨ update - 유저에 creatTime 추가

--- src/main/java/org/example/owncalendarserver/entity/User.java
+++ src/main/java/org/example/owncalendarserver/entity/User.java
@@ -4,6 +4,8 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.util.Date;
+
 @Entity
 @Getter
 @NoArgsConstructor
@@ -25,4 +27,15 @@ public class User {
     @Column(nullable = false)
     @Enumerated(value = EnumType.STRING)
     private UserRoleEnum role;
+
+    @Column(nullable = false)
+    private Date createTime;
+
+    public User (String nickName, String userName, String password, UserRoleEnum role) {
+        this.nickName = nickName;
+        this.userName = userName;
+        this.password = password;
+        this.role = role;
+        this.createTime = new Date();
+    }
 }
