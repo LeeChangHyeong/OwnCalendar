ffadaa6932b96c6c402fe77027b0a88ca35fc3f5
이창형 <71262367+LeeChangHyeong@users.noreply.github.com>
2024-05-28 15:42:31 +0900

    ✨ update - CommentController에 댓글을 delete 하도록 도와주는 메서드 생성

--- src/main/java/org/example/owncalendarserver/controller/CommentController.java
+++ src/main/java/org/example/owncalendarserver/controller/CommentController.java
@@ -4,7 +4,12 @@ import lombok.RequiredArgsConstructor;
 import org.example.owncalendarserver.dto.CommentRequestDto;
 import org.example.owncalendarserver.dto.CommentResponseDto;
 import org.example.owncalendarserver.service.CommentService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
@@ -17,8 +22,18 @@ public class CommentController {
         return commentService.createComment(requestDto, schedule_id);
     }
 
-    @PutMapping("/editComment/{comment_id}")
+    @PutMapping("/comment/{comment_id}")
     public CommentResponseDto editComment(@PathVariable Long comment_id, @RequestBody CommentRequestDto requestDto) {
             return commentService.editComment(requestDto, comment_id);
+    }
+
+    @DeleteMapping("/comment/{comment_id}")
+    public ResponseEntity<Map<String, Object>> deleteComment(@PathVariable Long comment_id, @RequestBody CommentRequestDto requestDto) {
+        commentService.deleteComment(requestDto, comment_id);
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("message", "삭제가 성공적으로 이루어졌습니다.");
+        response.put("status", HttpStatus.OK.value());
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 }
