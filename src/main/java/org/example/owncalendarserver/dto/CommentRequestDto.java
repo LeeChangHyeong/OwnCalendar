09c96b065fa94e7fc07f617796134fbaff981b0d
이창형 <71262367+LeeChangHyeong@users.noreply.github.com>
2024-05-28 16:30:25 +0900

    ✨ update - @valid 어노테이션으로 CommentRequestDto, ScheduleRequestDto 예외처리를 분리, 오류 메시지 작성

--- src/main/java/org/example/owncalendarserver/dto/CommentRequestDto.java
+++ src/main/java/org/example/owncalendarserver/dto/CommentRequestDto.java
@@ -1,9 +1,11 @@
 package org.example.owncalendarserver.dto;
 
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
 
 @Getter
 public class CommentRequestDto {
     private String content;
+    @NotEmpty(message = "user_id는 필수로 작성하셔야 합니다..")
     private String user_id;
 }
--- src/main/java/org/example/owncalendarserver/dto/ScheduleRequestDto.java
+++ src/main/java/org/example/owncalendarserver/dto/ScheduleRequestDto.java
@@ -1,11 +1,17 @@
 package org.example.owncalendarserver.dto;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 
 @Getter
 public class ScheduleRequestDto {
+    @NotEmpty(message = "title은 꼭 입력하셔야 합니다.")
     private String title;
+    @NotEmpty(message = "content는 꼭 입력하셔야 합니다.")
     private String content;
+    @NotEmpty(message = "name은 꼭 입력하셔야 합니다.")
     private String name;
+    @NotEmpty(message = "password는 꼭 입력하셔야 합니다.")
     private String password;
 }
