5bbadfa4ed2f38619ad3bcaac1e915cc44ed2524
이창형 <71262367+LeeChangHyeong@users.noreply.github.com>
2024-05-28 11:54:00 +0900

    ✨ update - CommentController에 댓글 수정을 도와주는 메서드 생성

--- src/main/java/org/example/owncalendarserver/controller/CommentController.java
+++ src/main/java/org/example/owncalendarserver/controller/CommentController.java
@@ -13,7 +13,12 @@ public class CommentController {
     private final CommentService commentService;
 
     @PostMapping("/comment/{schedule_id}")
-    public CommentResponseDto createComment(@RequestBody CommentRequestDto requestDto, @PathVariable Long schedule_id) {
+    public CommentResponseDto createComment(@PathVariable Long schedule_id, @RequestBody CommentRequestDto requestDto) {
         return commentService.createComment(requestDto, schedule_id);
+    }
+
+    @PostMapping("/editComment/{comment_id}")
+    public CommentResponseDto editComment(@PathVariable Long comment_id, @RequestBody CommentRequestDto requestDto) {
+            return commentService.editComment(requestDto, comment_id);
     }
 }
