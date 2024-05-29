c6e1831f63636359bc1507b0f2fad14ce7161693
이창형 <71262367+LeeChangHyeong@users.noreply.github.com>
2024-05-29 21:12:12 +0900

    🩹 fix - ScheduleResponseDto name 필드 삭제 (로그인 한 유저의 네임이 그대로 들어가면 되기 때문에)

--- src/main/java/org/example/owncalendarserver/dto/ScheduleResponseDto.java
+++ src/main/java/org/example/owncalendarserver/dto/ScheduleResponseDto.java
@@ -10,14 +10,12 @@ public class ScheduleResponseDto {
     private Long id;
     private String title;
     private String content;
-    private String name;
     private Date createDate;
 
     public ScheduleResponseDto(Schedule schedule) {
         this.id = schedule.getId();
         this.title = schedule.getTitle();
         this.content = schedule.getContent();
-        this.name = schedule.getName();
         this.createDate = schedule.getCreateDate();
     }
 }
