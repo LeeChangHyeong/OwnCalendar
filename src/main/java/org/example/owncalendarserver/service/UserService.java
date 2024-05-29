6f054864e8431d1235e3369e82b30de5777036e4
이창형 <71262367+LeeChangHyeong@users.noreply.github.com>
2024-05-29 12:52:26 +0900

    ✨ update - UserService에 로그인 로직을 담은 signup 메서드 구현

--- src/main/java/org/example/owncalendarserver/service/UserService.java
+++ src/main/java/org/example/owncalendarserver/service/UserService.java
@@ -1,11 +1,52 @@
 package org.example.owncalendarserver.service;
 
 import lombok.RequiredArgsConstructor;
+import org.example.owncalendarserver.dto.SignupRequestDto;
+import org.example.owncalendarserver.entity.User;
+import org.example.owncalendarserver.entity.UserRoleEnum;
 import org.example.owncalendarserver.repository.UserRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
+
+    // ADMIN_TOKEN
+    private final String ADMIN_TOKEN = "AAABnvxRVklrnYxKZ0aHgTBcXukeZygoC";
+
+    public void signup(SignupRequestDto requestDto) {
+        String nickName = requestDto.getNickName();
+        String userName = requestDto.getUserName();
+        String password = requestDto.getPassword();
+
+        // 닉네임 중복 확인
+        Optional<User> checkNickName = userRepository.findByNickName(nickName);
+        if (checkNickName.isPresent()) {
+            throw new IllegalStateException("중복된 닉네임이 존재합니다.");
+        }
+
+        // 회원 중복 확인
+        Optional<User> checkUserName = userRepository.findByUserName(userName);
+        if(checkUserName.isPresent()) {
+            throw new IllegalStateException("중복된 사용자가 있습니다.");
+        }
+
+        // 사용 권환 확인
+        UserRoleEnum role = UserRoleEnum.USER;
+        if (requestDto.isAdmin()) {
+            if (!ADMIN_TOKEN.equals(requestDto.getAdminToken())) {
+                throw new IllegalArgumentException("관리자 암호가 틀려 등록이 불가능합니다.");
+            }
+            role = UserRoleEnum.ADMIN;
+        }
+
+        // 사용자 등록
+        User user = new User(nickName, userName, password, role);
+        userRepository.save(user);
+
+    }
 }
