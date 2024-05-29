28df1ae63109894f00ab95449a8374250af04a8c
이창형 <71262367+LeeChangHyeong@users.noreply.github.com>
2024-05-29 18:43:11 +0900

    ✨ update - 댓글을 작성할때 이름을 따로 입력하는게 아닌 로그인 토큰에 있는 사용자의 이름으로 자동저장되게 변경

--- src/main/java/org/example/owncalendarserver/dto/CommentRequestDto.java
+++ src/main/java/org/example/owncalendarserver/dto/CommentRequestDto.java
@@ -6,6 +6,4 @@ import lombok.Getter;
 @Getter
 public class CommentRequestDto {
     private String content;
-    @NotEmpty(message = "user_id는 필수로 작성하셔야 합니다..")
-    private String user_id;
 }
--- src/main/java/org/example/owncalendarserver/service/CommentService.java
+++ src/main/java/org/example/owncalendarserver/service/CommentService.java
@@ -1,10 +1,13 @@
 package org.example.owncalendarserver.service;
 
+import io.jsonwebtoken.Claims;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.example.owncalendarserver.dto.CommentRequestDto;
 import org.example.owncalendarserver.dto.CommentResponseDto;
 import org.example.owncalendarserver.entity.Comment;
 import org.example.owncalendarserver.entity.Schedule;
+import org.example.owncalendarserver.jwt.JwtUtil;
 import org.example.owncalendarserver.repository.CommentRepository;
 import org.example.owncalendarserver.repository.ScheduleRepository;
 import org.springframework.stereotype.Service;
@@ -16,13 +19,25 @@ public class CommentService {
 
     private final CommentRepository commentRepository;
     private final ScheduleRepository scheduleRepository;
+    private final JwtUtil jwtUtil;
 
-    public CommentResponseDto createComment(CommentRequestDto requestDto, Long scheduleId) {
+    public CommentResponseDto createComment(CommentRequestDto requestDto, Long scheduleId, HttpServletRequest request) {
         Schedule schedule = scheduleRepository.findById(scheduleId).orElseThrow(() ->
                 new IllegalArgumentException("선택한 일정은 존재하지 않습니다.")
         );
 
-        Comment comment = new Comment(requestDto, schedule);
+        String token = jwtUtil.getJwtFromHeader(request);
+
+        if (!jwtUtil.validateToken(token)) {
+            throw new IllegalArgumentException("토큰값을 확인해주세요.");
+        }
+
+        // 토큰으로 유저 정보 받아오기
+        Claims user = jwtUtil.getUserInfoFromToken(token);
+
+        Comment comment = new Comment(requestDto, schedule, user.getSubject());
+
+        checkUser(comment.getUser_id(), request);
 
         Comment saveComment = commentRepository.save(comment);
 
@@ -32,24 +47,54 @@ public class CommentService {
     }
 
     @Transactional
-    public CommentResponseDto editComment(CommentRequestDto requestDto, Long commentId) {
+    public CommentResponseDto editComment(CommentRequestDto requestDto, Long commentId, HttpServletRequest request) {
         Comment comment = checkCommentWithId(commentId);
 
-        checkUserId(comment, requestDto);
+        // 토큰으로 유저 정보 받아오기
+        Claims user = getIdfromToken(request);
+
+        checkUserId(comment, user.getSubject());
+
+        checkUser(comment.getUser_id(), request);
 
         comment.update(requestDto);
 
         CommentResponseDto commentResponseDto = new CommentResponseDto(comment);
 
-        return  commentResponseDto;
+        return commentResponseDto;
     }
 
-    public void deleteComment(CommentRequestDto requestDto, Long commentId) {
+    // 토큰으로 아이디 들고옴
+    private Claims getIdfromToken(HttpServletRequest request) {
+        String token = jwtUtil.getJwtFromHeader(request);
+
+        if (!jwtUtil.validateToken(token)) {
+            throw new IllegalArgumentException("토큰값을 확인해주세요.");
+        }
+
+        return jwtUtil.getUserInfoFromToken(token);
+    }
+
+    public void deleteComment(Long commentId, HttpServletRequest request) {
         Comment comment = checkCommentWithId(commentId);
 
-        checkUserId(comment, requestDto);
+        // 토큰으로 유저 정보 받아오기
+        Claims user = getIdfromToken(request);
+
+        checkUserId(comment, user.getSubject());
+
+        checkUser(comment.getUser_id(), request);
 
         commentRepository.delete(comment);
+    }
+
+    private void checkUser(String id, HttpServletRequest request) {
+        // 토큰으로 유저 정보 받아오기
+        Claims user = getIdfromToken(request);
+
+        if (!user.getSubject().equals(id)) {
+            throw new IllegalArgumentException("스케쥴 작성자가 아니라 삭제가 불가능합니다.");
+        }
     }
 
     // 있는 schedule인지 체크
@@ -60,10 +105,9 @@ public class CommentService {
     }
 
     // 있는 comment인지 체크
-    public void checkUserId(Comment comment, CommentRequestDto requestDto) {
-      if(!comment.getUser_id().equals(requestDto.getUser_id())) {
-          throw new IllegalStateException("사용자가 일치하지 않습니다.");
-      }
+    public void checkUserId(Comment comment, String userId) {
+        if (!comment.getUser_id().equals(userId)) {
+            throw new IllegalStateException("사용자가 일치하지 않습니다.");
+        }
     }
-
 }
